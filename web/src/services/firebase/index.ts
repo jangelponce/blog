c3175f73417a169c543ded41ecc4c72a9ccf65ee
Angel Ponce <j.angelponce.esc@gmail.com>
2022-11-23 15:49:07 -0700

    Adding Firebase env vars to Zod schema

---
+++ web/src/services/firebase/index.ts
@@ -0,0 +1,19 @@
+import { env } from "../../env/server.mjs";
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: env.FIREBASE_API_KEY,
+  authDomain: env.FIREBASE_AUTH_DOMAIN,
+  projectId: env.FIREBASE_PROJECT_ID,
+  storageBucket: env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: env.FIREBASE_APP_ID,
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
